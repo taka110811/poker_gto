01c3444cd5db835e7d36ea7e5288bbec66730b51
taka110811 <babysteps1108@gmail.com>
2026-05-25 07:38:34 +0900

    Add unit tests for extracted modules

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("@playwright/test");
 module.exports = defineConfig({
   timeout: 120000,
   testDir: "./tests",
+  testMatch: "**/*.spec.js",
   use: {
     baseURL: "http://127.0.0.1:4173",
   },
